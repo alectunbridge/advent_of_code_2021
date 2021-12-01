07d75acaa0340b7edfc4e80c8d1a4accdd304282
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2021-12-01 16:45:21 +0000

    Day one - refactor

---
+++ src/main/java/advent_of_code_2021/Utils.java
@@ -0,0 +1,14 @@
+package advent_of_code_2021;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Utils {
+    public static List<String> readInputLinesFromFile(String filename) throws IOException, URISyntaxException {
+        return Files.lines(Paths.get(ClassLoader.getSystemResource(filename).toURI())).collect(Collectors.toList());
+    }
+}
