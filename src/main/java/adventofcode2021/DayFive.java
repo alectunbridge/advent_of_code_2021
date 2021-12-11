80f12355f69654d371d4e64de636d5a7dd6e3053
alec.tunbridge <alec.tunbridge@gmail.com>
2021-12-11 15:10:22 +0000

    DayFive part two - solved - but breaks part 1

--- src/main/java/adventofcode2021/DayFive.java
+++ src/main/java/adventofcode2021/DayFive.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DayFive {
 
@@ -22,21 +23,32 @@ public class DayFive {
 
     private List<CoordDayFive> generateCoords(int[] startCoords, int[] endCoords) {
         List<CoordDayFive> result = new ArrayList<>();
-        if(startCoords[0] == endCoords[0]){
+        if (startCoords[0] == endCoords[0]) {
             int xCoord = startCoords[0];
             int smallestY = Integer.min(startCoords[1], endCoords[1]);
             int largestY = Integer.max(startCoords[1], endCoords[1]);
-            for(int i = smallestY; i <= largestY; i++){
+            for (int i = smallestY; i <= largestY; i++) {
                 result.add(new CoordDayFive(xCoord, i));
             }
         }
-        if(startCoords[1] == endCoords[1]){
+        if (startCoords[1] == endCoords[1]) {
             int yCoord = startCoords[1];
             int smallestX = Integer.min(startCoords[0], endCoords[0]);
             int largestX = Integer.max(startCoords[0], endCoords[0]);
-            for(int i = smallestX; i <= largestX; i++){
+            for (int i = smallestX; i <= largestX; i++) {
                 result.add(new CoordDayFive(i, yCoord));
             }
+        }
+        if (Math.abs(startCoords[0] - endCoords[0]) == Math.abs(startCoords[1] - endCoords[1])) {
+            int noOfSteps = Math.abs(endCoords[0] - startCoords[0]);
+            int[] unitVector = {
+                    (endCoords[0] - startCoords[0]) / noOfSteps,
+                    (endCoords[1] - startCoords[1]) / noOfSteps};
+
+            IntStream.rangeClosed(0, noOfSteps).forEach(i -> {
+                result.add(new CoordDayFive(startCoords[0] + unitVector[0] * i, startCoords[1] + unitVector[1] * i));
+            });
+
         }
         return result;
     }
@@ -45,4 +57,5 @@ public class DayFive {
         Map<CoordDayFive, Long> countOfCoords = coordinates.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         return (int) countOfCoords.values().stream().filter(x -> x > 1).count();
     }
+
 }
--- src/test/java/adventofcode2021/DayFiveTest.java
+++ src/test/java/adventofcode2021/DayFiveTest.java
@@ -28,4 +28,22 @@ public class DayFiveTest {
         DayFive dayFive = new DayFive(Utils.readInputLinesFromFile("day_five.txt"));
         assertThat(dayFive.solvePart1()).isEqualTo(0);
     }
+
+    @Test
+    void part2Example() {
+        DayFive dayFive = new DayFive(List.of(
+                "0,9 -> 5,9",
+                "8,0 -> 0,8",
+                "9,4 -> 3,4",
+                "2,2 -> 2,1",
+                "7,0 -> 7,4",
+                "6,4 -> 2,0",
+                "0,9 -> 2,9",
+                "3,4 -> 1,4",
+                "0,0 -> 8,8",
+                "5,5 -> 8,2"
+        ));
+        assertThat(dayFive.solvePart1()).isEqualTo(12);
+    }
+
 }
