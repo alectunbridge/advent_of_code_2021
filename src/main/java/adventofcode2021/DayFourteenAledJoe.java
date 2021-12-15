131fafb2e48450cac134a49836e7b180137533dc
MojoJoJo86 <joseph.bradshaw@autotrader.co.uk>
2021-12-15 09:26:11 +0000

    Day14 Part 1 works

--- src/main/java/adventofcode2021/DayFourteenAledJoe.java
+++ src/main/java/adventofcode2021/DayFourteenAledJoe.java
@@ -1,8 +1,11 @@
 package adventofcode2021;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class DayFourteenAledJoe {
 
@@ -19,6 +22,24 @@ public class DayFourteenAledJoe {
 
 		}
 
+
+		for (int i = 0; i < 40; i++) {
+			initialPolymer = enhancePolymer(initialPolymer, instructions);
+		}
+
+		String[] values = initialPolymer.split("");
+
+		Map<String, Long> answers = Arrays.stream(values).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+		List<Long> list = answers.values().stream().sorted().collect(Collectors.toList());
+
+		return (int) (list.get(list.size()-1) - list.get(0));
+
+//		System.out.print(initialPolymer);
+	}
+
+
+	private String enhancePolymer(String initialPolymer, Map<String, String> instructions) {
 		for (int i = 0; i < initialPolymer.length()-1; i++) {
 
 			String firstPartDoNotAlter = initialPolymer.substring(0, i);
@@ -32,15 +53,12 @@ public class DayFourteenAledJoe {
 			if (value != null) {
 				newPolymer = keyWindow.charAt(0) + value + keyWindow.charAt(1);
 				initialPolymer = firstPartDoNotAlter + newPolymer + secondPartDoNotAlter;
-				i+=2;
+				i++;
 			}
-			System.out.print(initialPolymer);
 		}
-
-
-
-		return 0;
+		return initialPolymer;
 	}
 }
 
+//NCNBCHB
 //NCNBCHB
