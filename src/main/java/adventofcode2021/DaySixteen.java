b33b9763bedccc0fd645cb6357b5bd06dd084c06
alec.tunbridge <alec.tunbridge@gmail.com>
2021-12-18 21:06:00 +0000

    DaySixteen - Part 2 - minor refactor to separate parsing jobs

--- src/main/java/adventofcode2021/DaySixteen.java
+++ src/main/java/adventofcode2021/DaySixteen.java
@@ -16,48 +16,57 @@ public class DaySixteen {
     public Packet parsePacket(String binaryString) {
         int versionNumber = Integer.parseInt(binaryString.substring(0, 3), 2);
         int typeId = Integer.parseInt(binaryString.substring(3, 6), 2);
-        int stringIndex = 6;
-        Packet result = null;
 
         if (typeId == 4) {
-            String leadingBitValue = binaryString.substring(stringIndex, stringIndex + 1);
-            String value = "";
-            while (leadingBitValue.equals("1")) {
-                value += binaryString.substring(stringIndex + 1, stringIndex + 5);
-                stringIndex += 5;
-                leadingBitValue = binaryString.substring(stringIndex, stringIndex + 1);
-            }
+            return parseLiteralPacket(binaryString, versionNumber, typeId);
+        } else {
+            return parseOperatorPacket(binaryString, versionNumber, typeId);
+        }
+    }
+
+    private Packet parseOperatorPacket(String binaryString, int versionNumber, int typeId) {
+        int stringIndex = 6;
+        String lengthTypeId = binaryString.substring(stringIndex, stringIndex + 1);
+        stringIndex += 1;
+        int subPacketLength = 0;
+        int subPacketNumber = 0;
+        List<Packet> subPacketList = new ArrayList<>();
+
+        if (lengthTypeId.equals("0")) {
+            subPacketLength = Integer.parseInt(binaryString.substring(stringIndex, stringIndex + 15), 2);
+            stringIndex += 15;
+        } else {
+            subPacketNumber = Integer.parseInt(binaryString.substring(stringIndex, stringIndex + 11), 2);
+            stringIndex += 11;
+        }
+
+        int subPacketCount = 0;
+        int subPacketIndex = 0;
+        while (subPacketIndex < subPacketLength || subPacketCount < subPacketNumber) {
+            Packet subPacket = parsePacket(binaryString.substring(stringIndex + subPacketIndex));
+            subPacketList.add(subPacket);
+            subPacketIndex += subPacket.getLength();
+            subPacketCount++;
+        }
+        subPacketLength = subPacketLength == 0 ? subPacketIndex : subPacketLength;
+        subPacketNumber = subPacketNumber == 0 ? subPacketCount : subPacketNumber;
+        int length = stringIndex + subPacketLength;
+        return new OperatorPacket(versionNumber, typeId, length, subPacketLength, subPacketNumber, subPacketList);
+    }
+
+    private Packet parseLiteralPacket(String binaryString, int versionNumber, int typeId) {
+        Packet result;
+        int stringIndex = 6;
+        String leadingBitValue = binaryString.substring(stringIndex, stringIndex + 1);
+        String value = "";
+        while (leadingBitValue.equals("1")) {
             value += binaryString.substring(stringIndex + 1, stringIndex + 5);
             stringIndex += 5;
-            result = new LiteralPacket(versionNumber, typeId, stringIndex, Long.parseLong(value, 2));
-        } else {
-            String lengthTypeId = binaryString.substring(stringIndex, stringIndex + 1);
-            stringIndex += 1;
-            int subPacketLength = 0;
-            int subPacketNumber = 0;
-            List<Packet> subPacketList = new ArrayList<>();
-
-            if (lengthTypeId.equals("0")) {
-                subPacketLength = Integer.parseInt(binaryString.substring(stringIndex, stringIndex + 15), 2);
-                stringIndex += 15;
-            } else {
-                subPacketNumber = Integer.parseInt(binaryString.substring(stringIndex, stringIndex + 11), 2);
-                stringIndex += 11;
-            }
-
-            int subPacketCount = 0;
-            int subPacketIndex = 0;
-            while (subPacketIndex < subPacketLength || subPacketCount < subPacketNumber) {
-                Packet subPacket = parsePacket(binaryString.substring(stringIndex + subPacketIndex));
-                subPacketList.add(subPacket);
-                subPacketIndex += subPacket.getLength();
-                subPacketCount++;
-            }
-            subPacketLength = subPacketLength == 0 ? subPacketIndex : subPacketLength;
-            subPacketNumber = subPacketNumber == 0 ? subPacketCount : subPacketNumber;
-            result = new OperatorPacket(versionNumber, typeId, stringIndex + subPacketLength, subPacketLength, subPacketNumber, subPacketList);
+            leadingBitValue = binaryString.substring(stringIndex, stringIndex + 1);
         }
-//        System.out.println(binaryString.substring(0, stringIndex));
+        value += binaryString.substring(stringIndex + 1, stringIndex + 5);
+        stringIndex += 5;
+        result = new LiteralPacket(versionNumber, typeId, stringIndex, Long.parseLong(value, 2));
         return result;
     }
 }
