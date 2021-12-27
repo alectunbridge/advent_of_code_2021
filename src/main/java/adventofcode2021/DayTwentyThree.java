16e3f00285b09a441f30e890841e7e5efdfd24e7
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2021-12-27 11:15:46 +0000

    DayTwentyThree - Part 1 valid moves ok on example

--- src/main/java/adventofcode2021/DayTwentyThree.java
+++ src/main/java/adventofcode2021/DayTwentyThree.java
@@ -18,44 +18,44 @@ public class DayTwentyThree {
             int pieceX = piece[1];
             int pieceY = piece[2];
             boolean pieceIsInHallway = pieceY == 0;
-            if(pieceIsInHallway) {
+            if (pieceIsInHallway) {
                 int burrowX = getBurrowXCoordinate(piece[0]);
-                if(isEmpty(burrowX, 2)) {
-                    result.add(new int[]{piece[1], piece[2], burrowX, 2});
-                } else if(isEmpty(burrowX, 1) && state[2].charAt(burrowX) == piece[0]) {
-                    result.add(new int[]{piece[1], piece[2], burrowX, 1});
+                if (isHallwayClear(pieceX, burrowX)) {
+                    if (isEmpty(burrowX, 2)) {
+                        result.add(new int[]{piece[1], piece[2], burrowX, 2});
+                    } else if (isEmpty(burrowX, 1) && state[2].charAt(burrowX) == piece[0]) {
+                        result.add(new int[]{piece[1], piece[2], burrowX, 1});
+                    }
                 }
                 continue;
             }
             for (int x = 0; x < state[0].length(); x++) {
                 for (int y = 0; y < state.length; y++) {
-                    boolean entrance = (x == 2 || x == 4 || x == 6 || x == 8) && y == 0;
-                    if (entrance || !isEmpty(x,y) || x == pieceX || (y > 0 && getBurrowXCoordinate(piece[0]) != x)) {
+                    if (isEntrance(x, y) || !isEmpty(x, y) || x == pieceX || (y > 0 && getBurrowXCoordinate(piece[0]) != x)) {
                         continue;
                     }
-                    //check intervening locs
-                    if (pieceY == 1 || isEmpty(pieceX, 1)) {
-                        //we're good to get to hallway
-                        //are we good to move along it?
-                        boolean blocked = false;
-                        if (x != pieceX) { //we're changing x value
-                            int delta = (x - pieceX) / Math.abs(x - pieceX);
-                            for (int interX = pieceX + delta; interX != x; interX += delta) {
-                                if (!isEmpty(interX, 0)) {
-                                    blocked = true;
-                                    break;
-                                }
-                            }
-                        }
-
-                        if (!blocked) {
-                            result.add(new int[]{pieceX, pieceY, x, y});
-                        }
+                    boolean canGetToHallway = pieceY == 1 || isEmpty(pieceX, 1);
+                    if (canGetToHallway && isHallwayClear(pieceX, x)) {
+                        result.add(new int[]{pieceX, pieceY, x, y});
                     }
                 }
             }
         }
         return result;
+    }
+
+    private boolean isEntrance(int x, int y) {
+        return (x == 2 || x == 4 || x == 6 || x == 8) && y == 0;
+    }
+
+    private boolean isHallwayClear(int startX, int endX) {
+        int delta = (endX - startX) / Math.abs(endX - startX);
+        for (int interX = startX + delta; interX != endX; interX += delta) {
+            if (!isEmpty(interX, 0)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private int getBurrowXCoordinate(int name) {
@@ -88,7 +88,7 @@ public class DayTwentyThree {
 
     private boolean isSnugInBorrow(char character, int x, int y) {
         int burrowXCoordinate = getBurrowXCoordinate(character);
-        return (x == burrowXCoordinate) && (y==2 || (y==1 && state[2].charAt(x) == character));
+        return (x == burrowXCoordinate) && (y == 2 || (y == 1 && state[2].charAt(x) == character));
     }
 
 
--- src/test/java/adventofcode2021/DayTwentyThreeTest.java
+++ src/test/java/adventofcode2021/DayTwentyThreeTest.java
@@ -133,4 +133,148 @@ class DayTwentyThreeTest {
                 new int[]{8, 1, 10, 0}
         );
     }
+
+    @Test
+    void test1() {
+        DayTwentyThree dayTwentyThree = new DayTwentyThree(
+                new String[]{
+                       //01234567890
+                        ".....D.....",//0
+                        "##B#.#C#D##",//1
+                        "##A#B#C#A##"}//2
+        );
+        assertThat(dayTwentyThree.getAvailableMoves()).containsExactlyInAnyOrder(
+                //B moves
+                new int[]{2, 1, 0, 0},
+                new int[]{2, 1, 1, 0},
+                new int[]{2, 1, 3, 0},
+                new int[]{2, 1, 4, 1},
+
+                //C moves
+
+                //D moves
+                new int[]{8, 1, 7, 0},
+                new int[]{8, 1, 9, 0},
+                new int[]{8, 1, 10, 0}
+        );
+    }
+
+    @Test
+    void test2() {
+        DayTwentyThree dayTwentyThree = new DayTwentyThree(
+                new String[]{
+                       //01234567890
+                        ".....D.....",//0
+                        "##.#B#C#D##",//1
+                        "##A#B#C#A##"}//2
+        );
+        assertThat(dayTwentyThree.getAvailableMoves()).containsExactlyInAnyOrder(
+                //B moves
+
+                //C moves
+
+                //D moves
+                new int[]{8, 1, 7, 0},
+                new int[]{8, 1, 9, 0},
+                new int[]{8, 1, 10, 0}
+        );
+    }
+
+    @Test
+    void test3() {
+        DayTwentyThree dayTwentyThree = new DayTwentyThree(
+                new String[]{
+                       //01234567890
+                        ".....D.D...",//0
+                        "##.#B#C#.##",//1
+                        "##A#B#C#A##"}//2
+        );
+        assertThat(dayTwentyThree.getAvailableMoves()).containsExactlyInAnyOrder(
+                //A moves
+                new int[]{8, 2, 9, 0},
+                new int[]{8, 2, 10, 0}
+
+                //B moves
+
+                //C moves
+
+                //D moves
+        );
+    }
+
+    @Test
+    void test4() {
+        DayTwentyThree dayTwentyThree = new DayTwentyThree(
+                new String[]{
+                       //01234567890
+                        ".....D.D.A.",//0
+                        "##.#B#C#.##",//1
+                        "##A#B#C#.##"}//2
+        );
+        assertThat(dayTwentyThree.getAvailableMoves()).containsExactlyInAnyOrder(
+                //A moves
+
+
+                //B moves
+
+                //C moves
+
+                //D moves
+                new int[]{7, 0, 8, 2}
+        );
+    }
+
+    @Test
+    void test5() {
+        DayTwentyThree dayTwentyThree = new DayTwentyThree(
+                new String[]{
+                       //01234567890
+                        ".....D...A.",//0
+                        "##.#B#C#.##",//1
+                        "##A#B#C#D##"}//2
+        );
+        assertThat(dayTwentyThree.getAvailableMoves()).containsExactlyInAnyOrder(
+                //A moves
+
+                //B moves
+
+                //C moves
+
+                //D moves
+                new int[]{5, 0, 8, 1}
+        );
+    }
+
+    @Test
+    void test6() {
+        DayTwentyThree dayTwentyThree = new DayTwentyThree(
+                new String[]{
+                       //01234567890
+                        ".........A.",//0
+                        "##.#B#C#D##",//1
+                        "##A#B#C#D##"}//2
+        );
+        assertThat(dayTwentyThree.getAvailableMoves()).containsExactlyInAnyOrder(
+                //A moves
+                new int[]{9, 0, 2, 1}
+
+                //B moves
+
+                //C moves
+
+                //D moves
+        );
+    }
+
+    @Test
+    void test7() {
+        DayTwentyThree dayTwentyThree = new DayTwentyThree(
+                new String[]{
+                       //01234567890
+                        "...........",//0
+                        "##A#B#C#D##",//1
+                        "##A#B#C#D##"}//2
+        );
+        assertThat(dayTwentyThree.getAvailableMoves()).isEmpty();
+    }
 }
