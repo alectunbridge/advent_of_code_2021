7219dd7c18acfa3f624fbc9d3e02bad6d180e7a1
alectunbridge <alec.tunbridge@autotrader.co.uk>
2022-12-04 19:51:19 +0000

    Reduction works

--- src/main/java/adventofcode2021/DayEighteen.java
+++ src/main/java/adventofcode2021/DayEighteen.java
@@ -7,8 +7,10 @@ import java.util.regex.Pattern;
 import static java.lang.Integer.parseInt;
 
 public class DayEighteen {
-    public static final Pattern SIMPLE_PAIR_PATTERN = Pattern.compile("\\[(?<number1>\\d+),(?<number2>\\d+)](?<postfix>.*)");
-    public static final Pattern LEFT_STRING_PATTERN = Pattern.compile("(?<prefix>.*\\D)(?<number>\\d+)(?<postfix>\\D)*");
+    public static final Pattern SIMPLE_PAIR_PATTERN = Pattern
+            .compile("\\[(?<number1>\\d+),(?<number2>\\d+)](?<postfix>.*)");
+    public static final Pattern LEFT_STRING_PATTERN = Pattern
+            .compile("(?<prefix>.*\\D)(?<number>\\d+)(?<postfix>\\D*)");
     public static final Pattern RIGHT_STRING_PATTERN = Pattern.compile("(?<prefix>\\D*)(?<number>\\d+)(?<postfix>.*)");
     private List<String> input;
 
@@ -16,44 +18,69 @@ public class DayEighteen {
         this.input = input;
     }
 
-
     public String solvePart1() {
         String previousLine = null;
-        for (String line : input
-        ) {
-            previousLine = add(previousLine,line);
+        for (String line : input) {
+            previousLine = add(previousLine, line);
         }
         return previousLine;
     }
 
     private String add(String number1, String number2) {
-        if(number1 == null){
+        if (number1 == null) {
             return number2;
         }
-        String result = String.format("[%s,%s]",number1,number2);
-        result = explode(result);
+        String result = String.format("[%s,%s]", number1, number2);
+        while (true) {
+            String explodedResult = explode(result);
+            if (!explodedResult.equals(result)) {
+                result = explodedResult;
+                continue;
+            }
+            String splitResult = split(result);
+            if(splitResult.equals(result)){
+                break;
+            } else {
+                result = splitResult;
+            }
+        }
         return result;
+    }
+
+    private String split(String number) {
+        Pattern doubleDigitPattern = Pattern.compile("\\d\\d");
+        Matcher doubleDigitMatcher = doubleDigitPattern.matcher(number);
+        if (doubleDigitMatcher.find()) {
+            int leftNumber = parseInt(doubleDigitMatcher.group(0)) / 2;
+            int rightNumber = (int) Math.ceil(parseInt(doubleDigitMatcher.group(0)) / 2.0);
+            number = number.substring(0, doubleDigitMatcher.start()) + "[" + leftNumber + "," + rightNumber + "]"
+                    + number.substring(doubleDigitMatcher.end());
+        }
+        return number;
     }
 
     private String explode(String number) {
         int nesting = 0;
         for (int i = 0; i < number.length(); i++) {
             char currrentChar = number.charAt(i);
-            if(nesting == 5) {
-                if (Character.isDigit(currrentChar)){
-                    String leftString = number.substring(0,i-1);
-                    String rightString = number.substring(i-1);
+            if (nesting == 5) {
+                if (Character.isDigit(currrentChar)) {
+                    String leftString = number.substring(0, i - 1);
+                    String rightString = number.substring(i - 1);
                     Matcher pairMatcher = SIMPLE_PAIR_PATTERN.matcher(rightString);
                     pairMatcher.matches();
                     Matcher leftStringMatcher = LEFT_STRING_PATTERN.matcher(leftString);
-                    if(leftStringMatcher.matches()){
-                        int newNumber = parseInt(leftStringMatcher.group("number")) + parseInt(pairMatcher.group("number1"));
-                        leftString = leftStringMatcher.group("prefix")+newNumber+leftStringMatcher.group("postfix");
+                    if (leftStringMatcher.matches()) {
+                        int newNumber = parseInt(leftStringMatcher.group("number"))
+                                + parseInt(pairMatcher.group("number1"));
+                        leftString = leftStringMatcher.group("prefix") + newNumber + leftStringMatcher.group("postfix");
                     }
                     Matcher rightStringMatcher = RIGHT_STRING_PATTERN.matcher(pairMatcher.group("postfix"));
-                    if(rightStringMatcher.matches()){
-                        int newNumber = parseInt(rightStringMatcher.group("number")) + parseInt(pairMatcher.group("number2"));
-                        rightString = rightStringMatcher.group("prefix") + newNumber + rightStringMatcher.group("postfix");
+                    if (rightStringMatcher.matches()) {
+                        int newNumber = parseInt(rightStringMatcher.group("number"))
+                                + parseInt(pairMatcher.group("number2"));
+                        rightString = rightStringMatcher.group("prefix") + newNumber
+                                + rightStringMatcher.group("postfix");
                     } else {
                         rightString = pairMatcher.group("postfix");
                     }
@@ -61,7 +88,7 @@ public class DayEighteen {
                     return leftString + 0 + rightString;
                 }
             }
-            switch (currrentChar){
+            switch (currrentChar) {
                 case '[' -> nesting++;
                 case ']' -> nesting--;
             }
--- src/test/java/adventofcode2021/DayEighteenTest.java
+++ src/test/java/adventofcode2021/DayEighteenTest.java
@@ -1,5 +1,6 @@
 package adventofcode2021;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -41,6 +42,7 @@ public class DayEighteenTest {
     }
 
     @Test
+    @Disabled
     void explodeMultipleMatches(){
         DayEighteen dayEighteen = new DayEighteen(List.of(
             "[3,[2,[1,[7,3]]]]",
@@ -56,5 +58,41 @@ public class DayEighteenTest {
             "[9,[5,[4,[3,2]]]]"
         ));
         assertThat(dayEighteen.solvePart1()).isEqualTo("[[3,[2,[8,0]]],[9,[5,[7,0]]]]");
+    }
+
+    @Test
+    @Disabled
+    void split(){
+        DayEighteen dayEighteen = new DayEighteen(List.of(
+            "[[[0,7],4],[15,[0,13]]]",
+            "[1,1]"
+        ));
+        assertThat(dayEighteen.solvePart1()).isEqualTo("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]");
+    }
+
+    @Test
+    void reductionSmallExample(){
+        DayEighteen dayEighteen = new DayEighteen(List.of(
+            "[[[[4,3],4],4],[7,[[8,4],9]]]",
+            "[1,1]" 
+        ));
+        assertThat(dayEighteen.solvePart1()).isEqualTo("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]");
+    }
+
+    @Test
+    void reductionLargeExample(){
+        DayEighteen dayEighteen = new DayEighteen(List.of(
+            "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+            "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+            "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+            "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+            "[7,[5,[[3,8],[1,4]]]]",
+            "[[2,[2,2]],[8,[8,1]]]",
+            "[2,9]",
+            "[1,[[[9,3],9],[[9,0],[0,7]]]]",
+            "[[[5,[7,4]],7],1]",
+            "[[[[4,2],2],6],[8,7]]"
+        ));
+        assertThat(dayEighteen.solvePart1()).isEqualTo("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]");
     }
 }
